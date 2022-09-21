d27c55a11b3ee66b674c52b36b85dcdec02c8523
Lucy Suxo <lucy.elaine1@yahoo.com.br>
2022-09-21 16:51:21 -0300

    abstract ANIMAL

---
+++ Generatioon/src/POO2/Cavalo.java
@@ -0,0 +1,24 @@
+package POO2;
+
+public class Cavalo extends Animal{
+	
+
+	public Cavalo() {
+		super("Cavalo");
+	}
+	
+
+	public void Nome (String nomeanimal) {
+		System.out.println("Nome do cavalo: "+nomeanimal);
+	}
+	public void Idade (int idadeanimal) {
+		System.out.println("Idade: "+idadeanimal);
+	}
+	public void Som (String somanimal) {
+		System.out.println("Som que o seu cavalo emite: "+somanimal);
+	}
+	public void Corre () {
+		System.out.println("Uma das caracteristicas do seu cavalo é correr!");
+	}
+	
+}
---
+++ Generatioon/src/POO2/Preguica.java
@@ -0,0 +1,23 @@
+package POO2;
+
+public class Preguica extends Animal{
+	
+
+	public Preguica() {
+		super("Preguica"); }
+	
+	public void Nome (String nomeanimal) {
+		System.out.println("Nome do preguica: "+nomeanimal);
+	}
+	public void Idade (int idadeanimal) {
+		System.out.println("Idade: "+idadeanimal);
+	}
+	public void Som (String somanimal) {
+		System.out.println("Som que a sua preguica emite: "+somanimal);
+	}
+	public void Subirarvores () {
+		System.out.println("Uma das caracteristicas da sua preguica é subir em árvores!");
+	}
+
+	
+}
